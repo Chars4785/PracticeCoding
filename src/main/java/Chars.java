f49aae6fc8fc981a425a521c93d349a5be8a2b06
Unknown <9105lgm@naver.com>
2019-04-01 14:12:43 +0900

    init

--- src/main/java/Chars.java
+++ src/main/java/Chars.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Chars {
 
     public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-        int scanner.nextInt();
+        System.out.print("asd");
     }
 }
